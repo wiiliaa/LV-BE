8b9af72ec0444b0842ef5212ebc2f4b5a71febe7
dien <dienB1906652@student.ctu.edu.vn>
2023-11-23 11:57:38 +0700

    dien update truyen du lieu lon

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { json, urlencoded } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
+
   const config = new DocumentBuilder()
     .setTitle('DaDi')
     .setDescription('API DaDi')
