5916525a2ad9ba4347f5e7684a6280fd37d6bf4c
datduong <datduong010@gmail.com>
2023-12-07 12:31:13 +0700

    fix

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -166,11 +166,13 @@ export class ProductController {
     @Query('page') page: number = 1,
     @Query('pageSize') pageSize: number = 10,
     @Query('searchTerm') searchTerm?: string,
+    @Query('shopId') shopId?: number,
   ): Promise<{ products: Product[]; total: number }> {
     const { products, total } = await this.productService.findAll(
       page,
       pageSize,
       searchTerm,
+      shopId,
     );
     return { products, total };
   }
